5033ca308d16b9f6b22aa7eeaacb875bbb7186dd
Hamza Bin Aamir <u2023219@giki.edu.pk>
2024-10-17 01:40:32 +0500

    Reading datasets

---
+++ Analysis.py
@@ -0,0 +1,8 @@
+# importing the required libraries
+import pandas as pd # to read dataset
+import numpy as np # for calculations
+
+Dataset_F1 = pd.read_csv('./f1.csv')
+Dataset_IMDB = pd.read_csv('./imdb.csv')
+Dataset_NVDA = pd.read_csv('./NVDA.csv')
+
